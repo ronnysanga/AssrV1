3a9b20dc587b9264caf40941f56b5a58085cbec5
ronnysanga <85714316+ronnysanga@users.noreply.github.com>
2022-08-31 22:21:21 -0500

    pages 4

--- code.js
+++ code.js
@@ -1,29 +1,3 @@
-const express = require('express');
-const path = require('path');
-const http = require('http');
-
-
-
-const app = express();
-const server = http.createServer(app);
-
-const io = require('socket.io')(server);
-
-app.use(express.static(path.join(__dirname+'/public')));
-
-io.on('connection', function(socket){
-    socket.on('newuser', function(username){
-        socket.broadcast.emit('update', username + 'joined the conversation');
-    });
-    socket.on('exituser', function(username){
-        socket.broadcast.emit('update', username + 'left the conversation');
-    });
-    socket.on('chat', function(message){
-        socket.broadcast.emit('chat', message);
-    });
-});
-
-
 (function(){
 
     const app = document.querySelector('.app');
